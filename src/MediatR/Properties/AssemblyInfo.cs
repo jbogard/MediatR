7c90209b64c8b8f356655eb60b57910b9b5aa918
Jimmy Bogard <jimmy.bogard@gmail.com>
2014-03-04 15:35:17 -0600

    Basic mediation in

--- src/MediatR/Properties/AssemblyInfo.cs
+++ src/MediatR/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Resources;
+﻿using System;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -7,13 +8,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MediatR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Synchronous mediator implementation based on Common Service Locator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jimmy Bogard")]
 [assembly: AssemblyProduct("MediatR")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Jimmy Bogard 2014")]
 [assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
@@ -26,5 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: CLSCompliant(true)]
