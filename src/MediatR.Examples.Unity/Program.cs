edb91fe93760b1665512b9ef30a496c7bbac699a
Jimmy Bogard <jimmy.bogard@gmail.com>
2014-03-19 21:17:13 -0500

    Unity example, including variance

---
+++ src/MediatR.Examples.Unity/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MediatR.Examples.Unity
+{
+    using System;
+    using Microsoft.Practices.ServiceLocation;
+    using Microsoft.Practices.Unity;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var mediator = BuildMediator();
+
+            Runner.Run(mediator, Console.Out);
+        }
+
+        private static IMediator BuildMediator()
+        {
+            var container = new UnityContainer();
+            container.RegisterTypes(AllClasses.FromAssemblies(typeof (Ping).Assembly), WithMappings.FromAllInterfaces);
+            container.RegisterTypes(AllClasses.FromAssemblies(typeof (IMediator).Assembly), WithMappings.FromAllInterfaces);
+            container.RegisterInstance(Console.Out);
+
+            var serviceLocator = new UnityServiceLocator(container);
+            var serviceLocatorProvider = new ServiceLocatorProvider(() => serviceLocator);
+            container.RegisterInstance(serviceLocatorProvider);
+
+            var mediator = container.Resolve<IMediator>();
+
+            return mediator;
+        }
+    }
+}
