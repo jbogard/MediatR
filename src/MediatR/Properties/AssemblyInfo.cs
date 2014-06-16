7260d0d9314d99a60e2dbbcc5f80bd7c14b6182d
Jimmy Bogard <jimmy.bogard@gmail.com>
2014-06-16 05:10:35 -0500

    Upgrading to 1.3 of CSL

--- src/MediatR/Properties/AssemblyInfo.cs
+++ src/MediatR/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 [assembly: CLSCompliant(true)]
