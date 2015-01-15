5c5634fd8cc4a5225d5a4863d1d25c376ad57474
Vegar Vikan <vegar@vi-kan.net>
2015-01-15 22:34:31 +0100

    Initial attempt at lightinject implementation

---
+++ src/MediatR.Examples.LightInject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using LightInject;
+using LightInject.ServiceLocation;
+using Microsoft.Practices.ServiceLocation;
+
+namespace MediatR.Examples.LightInject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mediator = BuildMediator();
+
+            Runner.Run(mediator, Console.Out);
+
+            Console.ReadKey();
+        }
+
+        private static IMediator BuildMediator()
+        {
+            var serviceContainer = new ServiceContainer();
+            serviceContainer.Register<IMediator, Mediator>();
+            serviceContainer.RegisterAssembly(typeof (Ping).Assembly);
+            serviceContainer.RegisterAssembly(typeof(IMediator).Assembly);
+
+            serviceContainer.RegisterInstance(Console.Out);
+
+            var serviceLocator = new LightInjectServiceLocator(serviceContainer);
+            serviceContainer.RegisterInstance(new ServiceLocatorProvider(() => serviceLocator));
+
+            return serviceContainer.GetInstance<IMediator>(); 
+        }
+    }
+}
