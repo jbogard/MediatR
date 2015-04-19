4548f3dfe89201c85f0bd46b0e0e0c5771e4d600
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-04-19 18:35:58 -0400

    Bumping version

--- src/MediatR/Properties/AssemblyInfo.cs
+++ src/MediatR/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: CLSCompliant(true)]
