7dd313e7badac2b10b80bc9a0f9252c64a05d4a9
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-04-20 16:04:07 -0400

    Updating to latest dnxcore50

--- src/MediatR/Properties/AssemblyInfo.cs
+++ src/MediatR/Properties/AssemblyInfo.cs
@@ -23,4 +23,5 @@ using System.Reflection;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta-003")]
 [assembly: CLSCompliant(true)]
