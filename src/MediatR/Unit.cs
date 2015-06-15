9dc99882118356bde2ae19b6859264533f48ba40
Kristian Hellang <kristian@hellang.com>
2015-06-15 22:27:45 +0200

    Some adjustments to the Unit class

--- src/MediatR/Unit.cs
+++ src/MediatR/Unit.cs
@@ -5,26 +5,51 @@
     /// <summary>
     /// Represents a Void type, since Void is not a valid type in C#
     /// </summary>
-    public sealed class Unit : IComparable
+    public struct Unit : IEquatable<Unit>, IComparable<Unit>, IComparable
     {
         /// <summary>
         /// Default and only value of Unit type
         /// </summary>
         public static readonly Unit Value = new Unit();
 
-        public override int GetHashCode()
+        public int CompareTo(Unit other)
         {
             return 0;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj == null || obj is Unit;
         }
 
         int IComparable.CompareTo(object obj)
         {
             return 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public bool Equals(Unit other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit;
+        }
+
+        public static bool operator ==(Unit first, Unit second)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Unit first, Unit second)
+        {
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "()";
         }
     }
 }
