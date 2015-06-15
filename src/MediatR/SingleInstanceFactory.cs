c7b1bf0be68635cf7278d8da9561a5da21f16164
Kristian Hellang <kristian@hellang.com>
2015-06-15 22:35:22 +0200

    Filled in missing XML docs

--- src/MediatR/SingleInstanceFactory.cs
+++ src/MediatR/SingleInstanceFactory.cs
@@ -3,7 +3,8 @@ using System;
 namespace MediatR
 {
     /// <summary>
-    /// Factory method for creating single instances. Used to build instances of <see cref="IRequestHandler{TRequest,TResponse}"/> and <see cref="IAsyncRequestHandler{TRequest,TResponse}"/>
+    /// Factory method for creating single instances. Used to build instances of
+    /// <see cref="IRequestHandler{TRequest,TResponse}"/> and <see cref="IAsyncRequestHandler{TRequest,TResponse}"/>
     /// </summary>
     /// <param name="serviceType">Type of service to resolve</param>
     /// <returns>An instance of type <paramref name="serviceType" /></returns>
