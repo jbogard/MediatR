375faa00327b1b56e86e69bbf31d15d5bd036db7
dadhi <max.volkov@gmail.com>
2015-12-04 15:42:35 +0300

    added: Example with DryIoc

---
+++ src/MediatR.Examples.DryIoc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DryIoc;
+
+namespace MediatR.Examples.DryIoc
+{
+    class Program
+    {
+        static void Main()
+        {
+            var mediator = BuildMediator();
+
+            Runner.Run(mediator, Console.Out);
+
+            Console.ReadKey();
+        }
+
+        private static IMediator BuildMediator()
+        {
+            var container = new Container();
+
+            container.RegisterDelegate<SingleInstanceFactory>(r => serviceType => r.Resolve(serviceType));
+            container.RegisterDelegate<MultiInstanceFactory>(r => serviceType => r.ResolveMany(serviceType));
+            container.RegisterInstance(Console.Out);
+
+            container.RegisterMany(new[] {typeof (IMediator).Assembly, typeof (Ping).Assembly}, type => type.IsInterface); 
+
+            return container.Resolve<IMediator>();
+        }
+    }
+}
