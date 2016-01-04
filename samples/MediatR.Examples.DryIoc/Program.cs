b2b4badf1d15de11bf6166b0374a0947ced16f19
dadhi <max.volkov@gmail.com>
2016-01-04 12:20:28 +0300

    added: DryIoc sample targeting .NET Core

---
+++ samples/MediatR.Examples.DryIoc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using DryIoc;
+
+namespace MediatR.Examples.DryIoc
+{
+    class Program
+    {
+        static void Main()
+        {
+            var mediator = BuildMediator();
+
+            Runner.Run(mediator, Console.Out);
+
+            Console.ReadKey();
+        }
+
+        private static IMediator BuildMediator()
+        {
+            var container = new Container();
+
+            container.RegisterDelegate<SingleInstanceFactory>(r => serviceType => r.Resolve(serviceType));
+            container.RegisterDelegate<MultiInstanceFactory>(r => serviceType => r.ResolveMany(serviceType));
+            container.RegisterInstance(Console.Out);
+
+            container.RegisterMany(new[] { typeof(IMediator).GetAssembly(), typeof(Ping).GetAssembly() }, type => type.GetTypeInfo().IsInterface); 
+
+            return container.Resolve<IMediator>();
+        }
+    }
+}
