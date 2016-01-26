e90bfc73cb1af3e95873e5084920d699e180a286
Kristian Hellang <kristian@hellang.com>
2016-01-26 13:44:55 +0100

    Added ASP.NET Core sample using Scrutor for scanning

---
+++ samples/MediatR.Examples.AspNetCore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace MediatR.Examples.AspNetCore
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mediator = BuildMediator();
+            Runner.Run(mediator, Console.Out);
+            Console.ReadKey();
+        }
+
+        private static IMediator BuildMediator()
+        {
+            var services = new ServiceCollection();
+
+            services.AddScoped<SingleInstanceFactory>(p => t => p.GetRequiredService(t));
+            services.AddScoped<MultiInstanceFactory>(p => t => p.GetRequiredServices(t));
+
+            services.AddInstance(Console.Out);
+
+            // Use Scrutor to scan and register all
+            // classes as their implemented interfaces.
+            services.Scan(scan => scan
+                .FromAssembliesOf(typeof(IMediator), typeof(Ping))
+                .AddClasses()
+                .AsImplementedInterfaces());
+
+            var provider = services.BuildServiceProvider();
+
+            return provider.GetRequiredService<IMediator>();
+        }
+
+        private static IEnumerable<object> GetRequiredServices(this IServiceProvider provider, Type serviceType)
+        {
+            return (IEnumerable<object>) provider.GetRequiredService(typeof(IEnumerable<>).MakeGenericType(serviceType));
+        } 
+    }
+}
