e68b50eaf524abb2172974290111c376f0340957
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-05-24 13:45:40 -0500

    Build script using exec

--- Build.ps1
+++ Build.ps1
@@ -24,24 +24,43 @@ function EnsurePsbuildInstalled{
     }
 }
 
+# Taken from psake https://github.com/psake/psake
+
+<#
+.SYNOPSIS
+  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+  to see if an error occcured. If an error is detected then an exception is thrown.
+  This function allows you to run command-line programs without having to
+  explicitly check the $lastexitcode variable.
+.EXAMPLE
+  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+function Exec
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 EnsurePsbuildInstalled
 
-& dotnet restore
-if($LASTEXITCODE -ne 0) { exit 1 }
+exec { & dotnet restore }
 
-& Invoke-MSBuild
-if($LASTEXITCODE -ne 0) { exit 1 }    
+Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
+exec { & dotnet pack .\src\MediatR -c Release -o .\artifacts --version-suffix=$revision }
 
-& dotnet pack .\src\MediatR -c Release -o .\artifacts --version-suffix=$revision
-if($LASTEXITCODE -ne 0) { exit 1 }    
-
-& dotnet test .\test\MediatR.Tests -c Release
-if($LASTEXITCODE -ne 0) { exit 2 }
+exec { & dotnet test .\test\MediatR.Tests -c Release }
 
 
