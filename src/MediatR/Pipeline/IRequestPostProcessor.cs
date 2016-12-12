dbcf120a0fb5bb0cc2920b4f2e08586d8b768505
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-12-12 16:03:56 -0600

    Adding request post-processor pipeline stuff

---
+++ src/MediatR/Pipeline/IRequestPostProcessor.cs
@@ -0,0 +1,9 @@
+﻿namespace MediatR.Pipeline
+{
+    using System.Threading.Tasks;
+
+    public interface IRequestPostProcessor<in TRequest, in TResponse>
+    {
+        Task Process(TRequest request, TResponse response);
+    }
+}
---
+++ src/MediatR/Pipeline/RequestPostProcessorBehavior.cs
@@ -0,0 +1,25 @@
+﻿namespace MediatR.Pipeline
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class RequestPostProcessorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IRequestPostProcessor<TRequest, TResponse>> _postProcessors;
+
+        public RequestPostProcessorBehavior(IEnumerable<IRequestPostProcessor<TRequest, TResponse>> postProcessors)
+        {
+            _postProcessors = postProcessors;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
+        {
+            var response = await next();
+
+            await Task.WhenAll(_postProcessors.Select(p => p.Process(request, response)));
+
+            return response;
+        }
+    }
+}
