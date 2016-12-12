b6513235457c7d56a23fe1f07c774d81c51b398f
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-12-12 15:46:30 -0600

    Adding request pre-processor pipeline stuff

---
+++ src/MediatR/Pipeline/IRequestPreProcessor.cs
@@ -0,0 +1,9 @@
+﻿namespace MediatR.Pipeline
+{
+    using System.Threading.Tasks;
+
+    public interface IRequestPreProcessor<in TRequest>
+    {
+        Task Process(TRequest request);
+    }
+}
---
+++ test/MediatR.Tests/Pipeline/RequestPreProcessorTests.cs
@@ -0,0 +1,66 @@
+﻿namespace MediatR.Tests.Pipeline
+{
+    using System.Threading.Tasks;
+    using MediatR.Pipeline;
+    using Shouldly;
+    using StructureMap;
+    using Xunit;
+
+    public class RequestPreProcessorTests
+    {
+        public class Ping : IAsyncRequest<Pong>
+        {
+            public string Message { get; set; }
+        }
+
+        public class Pong
+        {
+            public string Message { get; set; }
+        }
+
+        public class PingHandler : IAsyncRequestHandler<Ping, Pong>
+        {
+            public Task<Pong> Handle(Ping message)
+            {
+                return Task.FromResult(new Pong { Message = message.Message + " Pong" });
+            }
+        }
+
+        public class PingPreProcessor : IRequestPreProcessor<Ping>
+        {
+            public Task Process(Ping request)
+            {
+                request.Message = request.Message + " Ping";
+
+                return Task.FromResult(0);
+            }
+        }
+
+        [Fact]
+        public async Task Should_run_preprocessors()
+        {
+            var container = new Container(cfg =>
+            {
+                cfg.Scan(scanner =>
+                {
+                    scanner.AssemblyContainingType(typeof(AsyncPublishTests));
+                    scanner.IncludeNamespaceContainingType<Ping>();
+                    scanner.WithDefaultConventions();
+                    scanner.AddAllTypesOf(typeof(IAsyncRequestHandler<,>));
+                    scanner.AddAllTypesOf(typeof(IRequestPreProcessor<>));
+                });
+                cfg.For(typeof(IPipelineBehavior<,>)).Add(typeof(RequestPreProcessorBehavior<,>));
+                cfg.For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+                cfg.For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+                cfg.For<IMediator>().Use<Mediator>();
+            });
+
+            var mediator = container.GetInstance<IMediator>();
+
+            var response = await mediator.SendAsync(new Ping { Message = "Ping" });
+
+            response.Message.ShouldBe("Ping Ping Pong");
+        }
+
+    }
+}
