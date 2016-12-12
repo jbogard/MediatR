5b5e82c22235f118e7f0de12c9b82001705a9ac2
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-12-12 15:46:30 -0600

    Adding request pre-processor pipeline stuff

---
+++ src/MediatR/Pipeline/RequestPreProcessorBehavior.cs
@@ -0,0 +1,23 @@
+﻿namespace MediatR.Pipeline
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class RequestPreProcessorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IRequestPreProcessor<TRequest>> _preProcessors;
+
+        public RequestPreProcessorBehavior(IEnumerable<IRequestPreProcessor<TRequest>> preProcessors)
+        {
+            _preProcessors = preProcessors;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
+        {
+            await Task.WhenAll(_preProcessors.Select(p => p.Process(request)));
+
+            return await next();
+        }
+    }
+}
