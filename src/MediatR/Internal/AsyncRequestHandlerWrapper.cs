56cf5eedf9d2d4c24ef73cf65f6522f33a5368db
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-12-20 15:51:26 -0600

    Removing unused classes

--- src/MediatR/Internal/AsyncRequestHandlerWrapper.cs
+++ src/MediatR/Internal/AsyncRequestHandlerWrapper.cs
@@ -2,23 +2,9 @@ using System.Threading.Tasks;
 
 namespace MediatR.Internal
 {
-    internal abstract class AsyncRequestHandlerWrapper
-    {
-        public abstract Task Handle(IRequest message, object handler);
-    }
-
     internal abstract class AsyncRequestHandlerWrapper<TResult>
     {
         public abstract Task<TResult> Handle(IRequest<TResult> message, object handler);
-    }
-
-    internal class AsyncRequestHandlerWrapperImpl<TRequest> : AsyncRequestHandlerWrapper
-        where TRequest : IRequest
-    {
-        public override Task Handle(IRequest message, object handler)
-        {
-            return ((IAsyncRequestHandler<TRequest>)handler).Handle((TRequest)message);
-        }
     }
 
     internal class AsyncRequestHandlerWrapperImpl<TCommand, TResult> : AsyncRequestHandlerWrapper<TResult>
--- src/MediatR/Internal/CancellableAsyncRequestHandlerWrapper.cs
+++ src/MediatR/Internal/CancellableAsyncRequestHandlerWrapper.cs
@@ -3,23 +3,9 @@ using System.Threading.Tasks;
 
 namespace MediatR.Internal
 {
-    internal abstract class CancellableAsyncRequestHandlerWrapper
-    {
-        public abstract Task Handle(IRequest message, CancellationToken cancellationToken, object handler);
-    }
-
     internal abstract class CancellableAsyncRequestHandlerWrapper<TResult>
     {
         public abstract Task<TResult> Handle(IRequest<TResult> message, CancellationToken cancellationToken, object handler);
-    }
-
-    internal class CancellableAsyncRequestHandlerWrapperImpl<TRequest> : CancellableAsyncRequestHandlerWrapper
-        where TRequest : IRequest
-    {
-        public override Task Handle(IRequest message, CancellationToken cancellationToken, object handler)
-        {
-            return ((ICancellableAsyncRequestHandler<TRequest>)handler).Handle((TRequest)message, cancellationToken);
-        }
     }
 
     internal class CancellableAsyncRequestHandlerWrapperImpl<TRequest, TResponse> : CancellableAsyncRequestHandlerWrapper<TResponse>
