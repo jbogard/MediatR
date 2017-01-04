684d61d9cdbfea3c6736cdacea4fa455737d9e7d
Jimmy Bogard <jimmy.bogard@gmail.com>
2017-01-04 15:35:22 -0600

    Documentation

--- src/MediatR/Pipeline/IRequestPreProcessor.cs
+++ src/MediatR/Pipeline/IRequestPreProcessor.cs
@@ -2,8 +2,17 @@
 {
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Defined a request pre-processor for a handler
+    /// </summary>
+    /// <typeparam name="TRequest">Request type</typeparam>
     public interface IRequestPreProcessor<in TRequest>
     {
+        /// <summary>
+        /// Process method executes before calling the Handle method on your handler
+        /// </summary>
+        /// <param name="request">Incoming request</param>
+        /// <returns>An awaitable task</returns>
         Task Process(TRequest request);
     }
 }
