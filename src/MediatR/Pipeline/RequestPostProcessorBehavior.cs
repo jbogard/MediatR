080d5c7f3d9b9bba8a1c2b0c854995ea108e6e5f
Michael Croes <m.croes@viscon.eu>
2017-02-15 13:09:08 +0100

    Apply ConfigureAwait(false) on all awaits
    See https://msdn.microsoft.com/en-us/magazine/jj991977.aspx for
    information regarding async/await. Pipeline behavior that was expecting
    to run on the SynchronizationContext that the IMediator call was made
    from will now have to take care of the switch back to the original
    SynchronizationContext. Some examples of doing that can be found at
    https://blogs.msdn.microsoft.com/pfxteam/2011/01/13/await-anything/.

--- src/MediatR/Pipeline/RequestPostProcessorBehavior.cs
+++ src/MediatR/Pipeline/RequestPostProcessorBehavior.cs
@@ -20,9 +20,9 @@
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next)
         {
-            var response = await next();
+            var response = await next().ConfigureAwait(false);
 
-            await Task.WhenAll(_postProcessors.Select(p => p.Process(request, response)));
+            await Task.WhenAll(_postProcessors.Select(p => p.Process(request, response))).ConfigureAwait(false);
 
             return response;
         }
