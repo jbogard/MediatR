a58b697c61a7ae22280dbfce902dfe9eb6d661b3
Fred Machado <fredisoft@gmail.com>
2017-05-16 08:21:05 +1000

    Fix path to artifacts

--- Build.ps1
+++ Build.ps1
@@ -22,7 +22,7 @@ function Exec
     }
 }
 
-if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+if(Test-Path .\artifacts) { Remove-Item .\src\MediatR\artifacts -Force -Recurse }
 
 exec { & dotnet restore }
 
