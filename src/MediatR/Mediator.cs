c72b0591753b34aa6c441cc6841d5a7224cde0e9
Stian Kroknes <stian.kroknes@gmail.com>
2017-09-12 20:53:50 +0200

    Added request/notification null guard in Mediator-impl
    Avoid NullReferenceException and instead get a ArgumentNullException.

--- src/MediatR/Mediator.cs
+++ src/MediatR/Mediator.cs
@@ -1,4 +1,4 @@
-﻿namespace MediatR
+namespace MediatR
 {
     using Internal;
     using System;
@@ -31,6 +31,11 @@
 
         public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default(CancellationToken))
         {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
             var requestType = request.GetType();
 
             var handler = (RequestHandler<TResponse>)_requestHandlers.GetOrAdd(requestType,
@@ -41,6 +46,11 @@
 
         public Task Send(IRequest request, CancellationToken cancellationToken = default(CancellationToken))
         {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
             var requestType = request.GetType();
 
             var handler = _voidRequestHandlers.GetOrAdd(requestType,
@@ -52,6 +62,11 @@
         public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default(CancellationToken))
             where TNotification : INotification
         {
+            if (notification == null)
+            {
+                throw new ArgumentNullException(nameof(notification));
+            }
+
             var notificationType = notification.GetType();
             var handler = _notificationHandlers.GetOrAdd(notificationType,
                 t => (NotificationHandler)Activator.CreateInstance(typeof(NotificationHandlerImpl<>).MakeGenericType(notificationType)));
