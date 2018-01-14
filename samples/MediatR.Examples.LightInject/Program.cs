9cdf38c894240c088709f29fbc94847eb590a05e
Bernhard Richter <bernhard.richter@gmail.com>
2018-01-14 22:27:02 +0100

    Updated LightInject to 5.1.2 adding support for ordered services

--- samples/MediatR.Examples.LightInject/Program.cs
+++ samples/MediatR.Examples.LightInject/Program.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -22,8 +20,7 @@ namespace MediatR.Examples.LightInject
         private static IMediator BuildMediator(WrappingWriter writer)
         {
             var serviceContainer = new ServiceContainer();
-            serviceContainer.Register<IMediator, Mediator>();
-            serviceContainer.RegisterAssembly(typeof(IMediator).GetTypeInfo().Assembly, (serviceType, implementingType) => !serviceType.GetTypeInfo().IsClass);
+            serviceContainer.Register<IMediator, Mediator>();            
             serviceContainer.RegisterInstance<TextWriter>(writer);
 
             serviceContainer.RegisterAssembly(typeof(Ping).GetTypeInfo().Assembly, (serviceType, implementingType) =>
@@ -33,17 +30,25 @@ namespace MediatR.Examples.LightInject
                     serviceType.GetGenericTypeDefinition() == typeof(IRequestHandler<>) ||
                     serviceType.GetGenericTypeDefinition() == typeof(INotificationHandler<>)
                 ));
-            
-            //Pipeline
-            //TODO: can't get the order right..
-            serviceContainer.Register(typeof(IPipelineBehavior<,>), typeof(RequestPreProcessorBehavior<,>));
-            serviceContainer.Register(typeof(IPipelineBehavior<,>), typeof(RequestPostProcessorBehavior<,>));
-            serviceContainer.Register(typeof(IPipelineBehavior<,>), typeof(GenericPipelineBehavior<,>));
-            serviceContainer.Register(typeof(IRequestPreProcessor<>), typeof(GenericRequestPreProcessor<>));
-            serviceContainer.Register(typeof(IRequestPostProcessor<,>), typeof(GenericRequestPostProcessor<,>));
-            serviceContainer.Register(typeof(IRequestPostProcessor<,>), typeof(ConstrainedRequestPostProcessor<,>));
-            serviceContainer.Register(typeof(INotificationHandler<>), typeof(ConstrainedPingedHandler<>));
+                    
+            serviceContainer.RegisterOrdered(typeof(IPipelineBehavior<,>),
+                new[]
+                {
+                    typeof(RequestPreProcessorBehavior<,>),
+                    typeof(RequestPostProcessorBehavior<,>),
+                    typeof(GenericPipelineBehavior<,>)
+                }, type => new PerContainerLifetime());
 
+            
+            serviceContainer.RegisterOrdered(typeof(IRequestPostProcessor<,>),
+                new[]
+                {
+                    typeof(GenericRequestPostProcessor<,>),
+                    typeof(ConstrainedRequestPostProcessor<,>)
+                }, type => new PerContainerLifetime());
+                   
+            serviceContainer.Register(typeof(IRequestPreProcessor<>), typeof(GenericRequestPreProcessor<>));            
+            serviceContainer.Register(typeof(INotificationHandler<>), typeof(ConstrainedPingedHandler<>));
 
             serviceContainer.Register<SingleInstanceFactory>(fac => fac.GetInstance);
             serviceContainer.Register<MultiInstanceFactory>(fac => fac.GetAllInstances);
