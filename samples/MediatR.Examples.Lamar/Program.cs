720f8f14169d07017f3a82c73f50b68fb9154686
Jimmy Bogard <jimmy.bogard@gmail.com>
2018-04-03 08:46:50 -0500

    Switching from BlueMilk to Lamar

--- samples/MediatR.Examples.Lamar/Program.cs
+++ samples/MediatR.Examples.Lamar/Program.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using BlueMilk;
+using Lamar;
 using MediatR.Pipeline;
 
 namespace MediatR.Examples.BlueMilk
