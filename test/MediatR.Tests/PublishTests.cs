ff0a2533c94caf127677f9a9e17ba21af2682acd
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-12-19 07:41:24 +0100

    Test of inheritance

--- test/MediatR.Tests/PublishTests.cs
+++ test/MediatR.Tests/PublishTests.cs
@@ -18,7 +18,10 @@ namespace MediatR.Tests
         {
             public string Message { get; set; }
         }
-
+        public class Ping2 : Ping
+        {
+            public string AdditionalInfo { get; set; }
+        }
         public class PongHandler : INotificationHandler<Ping>
         {
             private readonly TextWriter _writer;
@@ -77,7 +80,34 @@ namespace MediatR.Tests
             result.ShouldContain("Ping Pong");
             result.ShouldContain("Ping Pung");
         }
+        [Fact]
+        public async Task Should_resolve_main_handler_for_inherited_message()
+        {
+            var builder = new StringBuilder();
+            var writer = new StringWriter(builder);
 
+            var container = new Container(cfg =>
+            {
+                cfg.Scan(scanner =>
+                {
+                    scanner.AssemblyContainingType(typeof(PublishTests));
+                    scanner.IncludeNamespaceContainingType<Ping>();
+                    scanner.WithDefaultConventions();
+                    scanner.AddAllTypesOf(typeof (INotificationHandler<>));
+                });
+                cfg.For<TextWriter>().Use(writer);
+                cfg.For<IMediator>().Use<Mediator>();
+                cfg.For<ServiceFactory>().Use<ServiceFactory>(ctx => t => ctx.GetInstance(t));
+            });
+
+            var mediator = container.GetInstance<IMediator>();
+
+            await mediator.Publish(new Ping2 { Message = "Ping", AdditionalInfo="<>" });
+
+            var result = builder.ToString().Split(new [] {Environment.NewLine}, StringSplitOptions.None);
+            result.ShouldContain("Ping Pong");
+            result.ShouldContain("Ping Pung");
+        }
         [Fact]
         public async Task Should_resolve_main_handler_when_object_is_passed()
         {
@@ -107,7 +137,35 @@ namespace MediatR.Tests
             result.ShouldContain("Ping Pong");
             result.ShouldContain("Ping Pung");
         }
+        [Fact]
+        public async Task Should_resolve_main_handler_when_inherited_object_is_passed()
+        {
+            var builder = new StringBuilder();
+            var writer = new StringWriter(builder);
 
+            var container = new Container(cfg =>
+            {
+                cfg.Scan(scanner =>
+                {
+                    scanner.AssemblyContainingType(typeof(PublishTests));
+                    scanner.IncludeNamespaceContainingType<Ping>();
+                    scanner.WithDefaultConventions();
+                    scanner.AddAllTypesOf(typeof (INotificationHandler<>));
+                });
+                cfg.For<TextWriter>().Use(writer);
+                cfg.For<IMediator>().Use<Mediator>();
+                cfg.For<ServiceFactory>().Use<ServiceFactory>(ctx => t => ctx.GetInstance(t));
+            });
+
+            var mediator = container.GetInstance<IMediator>();
+
+            object message = new Ping2 { Message = "Ping", AdditionalInfo="<>" };
+            await mediator.Publish(message);
+
+            var result = builder.ToString().Split(new [] {Environment.NewLine}, StringSplitOptions.None);
+            result.ShouldContain("Ping Pong");
+            result.ShouldContain("Ping Pung");
+        }
         public class SequentialMediator : Mediator
         {
             public SequentialMediator(ServiceFactory serviceFactory)
