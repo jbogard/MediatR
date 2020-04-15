3c78c876d3f4852a779fc2f45a005e635712c59f
Dasith Wijesiriwardena <dasiths@hotmail.com>
2020-04-15 11:00:19 +1000

    attribute description

--- src/MediatR/Pipeline/PipelinePriorityAttribute.cs
+++ src/MediatR/Pipeline/PipelinePriorityAttribute.cs
@@ -2,7 +2,10 @@
 
 namespace MediatR.Pipeline
 {
-    [AttributeUsage((AttributeTargets.Class | AttributeTargets.Interface), AllowMultiple = false, Inherited = true)]
+    /// <summary>
+    /// Controls the order of execution for <see cref="IPipelineBehavior{TRequest,TResponse}"/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     public class PipelinePriorityAttribute : Attribute
     {
         public PipelinePriorityAttribute(int priority)
@@ -10,6 +13,9 @@ namespace MediatR.Pipeline
             Priority = priority;
         }
 
+        /// <summary>
+        /// Priority of execution. Higher the the number the higher the priority. See <see cref="PipelinePriorityOrder"/> for predefined priorities.
+        /// </summary>
         public int Priority { get; set; }
     }
 
@@ -18,9 +24,9 @@ namespace MediatR.Pipeline
         public const int Normal = 0;
         public const int High = 100;
         public const int Low = -100;
-        internal const int RequestPreProcessor = int.MaxValue;
-        internal const int RequestPostProcessor = int.MaxValue - 1;
-        internal const int RequestExceptionActionProcessor = int.MaxValue - 2;
-        internal const int RequestExceptionProcessor = int.MaxValue - 3;
+        public const int RequestPreProcessor = int.MaxValue;
+        public const int RequestPostProcessor = int.MaxValue - 1;
+        public const int RequestExceptionActionProcessor = int.MaxValue - 2;
+        public const int RequestExceptionProcessor = int.MaxValue - 3;
     }
 }
