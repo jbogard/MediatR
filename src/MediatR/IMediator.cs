8e824a81b3563ae615777b2f03343e624c6f9674
Ken Bonny <bonny.ken+github@gmail.com>
2020-09-24 20:10:15 +0200

    move publishing events to their own interface to separate this functionality

--- src/MediatR/IMediator.cs
+++ src/MediatR/IMediator.cs
@@ -1,29 +1,11 @@
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace MediatR
 {
     /// <summary>
     /// Defines a mediator to encapsulate request/response and publishing interaction patterns
     /// </summary>
-    public interface IMediator : ISender
+    public interface IMediator : ISender, IPublisher
     {
-        /// <summary>
-        /// Asynchronously send a notification to multiple handlers
-        /// </summary>
-        /// <param name="notification">Notification object</param>
-        /// <param name="cancellationToken">Optional cancellation token</param>
-        /// <returns>A task that represents the publish operation.</returns>
-        Task Publish(object notification, CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Asynchronously send a notification to multiple handlers
-        /// </summary>
-        /// <param name="notification">Notification object</param>
-        /// <param name="cancellationToken">Optional cancellation token</param>
-        /// <returns>A task that represents the publish operation.</returns>
-        Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
-            where TNotification : INotification;
     }
 }
---
+++ src/MediatR/IPublisher.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatR
+{
+    public interface IPublisher
+    {
+        /// <summary>
+        /// Asynchronously send a notification to multiple handlers
+        /// </summary>
+        /// <param name="notification">Notification object</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
+        /// <returns>A task that represents the publish operation.</returns>
+        Task Publish(object notification, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously send a notification to multiple handlers
+        /// </summary>
+        /// <param name="notification">Notification object</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
+        /// <returns>A task that represents the publish operation.</returns>
+        Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+            where TNotification : INotification;
+    }
+}
