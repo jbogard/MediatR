a9a26fb46e2f0f356c2c940b0f0267cb920e3cda
Ken Bonny <bonny.ken+github@gmail.com>
2020-09-24 20:23:26 +0200

    add test to resolve mediator via the specific publisher interface

--- test/MediatR.Tests/PublishTests.cs
+++ test/MediatR.Tests/PublishTests.cs
@@ -182,5 +182,34 @@ namespace MediatR.Tests
             result.ShouldContain("Ping Pong");
             result.ShouldContain("Ping Pung");
         }
+
+        [Fact]
+        public async Task Should_resolve_main_handler_by_specific_interface()
+        {
+            var builder = new StringBuilder();
+            var writer = new StringWriter(builder);
+
+            var container = new Container(cfg =>
+            {
+                cfg.Scan(scanner =>
+                {
+                    scanner.AssemblyContainingType(typeof(PublishTests));
+                    scanner.IncludeNamespaceContainingType<Ping>();
+                    scanner.WithDefaultConventions();
+                    scanner.AddAllTypesOf(typeof (INotificationHandler<>));
+                });
+                cfg.For<TextWriter>().Use(writer);
+                cfg.For<IPublisher>().Use<Mediator>();
+                cfg.For<ServiceFactory>().Use<ServiceFactory>(ctx => t => ctx.GetInstance(t));
+            });
+
+            var mediator = container.GetInstance<IPublisher>();
+
+            await mediator.Publish(new Ping { Message = "Ping" });
+
+            var result = builder.ToString().Split(new [] {Environment.NewLine}, StringSplitOptions.None);
+            result.ShouldContain("Ping Pong");
+            result.ShouldContain("Ping Pung");
+        }
     }
 }
