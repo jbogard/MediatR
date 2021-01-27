bc80eb267799c3e26244f3a768072e6f5213a28f
Saeed Ganji <shahabganji@gmail.com>
2021-01-28 00:26:31 +0100

    add example for GraceIoC
    Adds an example of how to configure MediatR with GraceIoC

---
+++ samples/MediatR.Examples.GraceIoc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Grace.DependencyInjection.Extensions;
+
+namespace MediatR.Examples.GraceIoc
+{
+    class Program
+    {
+        static Task Main()
+        {
+            var writer = new WrappingWriter(Console.Out);
+            var mediator = BuildMediator(writer);
+
+            return Runner.Run(mediator, writer, "GraceIoC");
+        }
+
+        private static IMediator BuildMediator(WrappingWriter writer)
+        {
+            var container = new DependencyInjectionContainer();
+            
+            container.Configure(c =>
+            {
+                c.ExportFunc<ServiceFactory>(scope => scope.Locate);
+            
+                c.ExportInstance<TextWriter>(writer);
+            
+                //Pipeline works out of the box here
+                c.ExportAssemblies(
+                        new[] {typeof(IMediator).Assembly, typeof(Ping).Assembly})
+                    .ByInterfaces();
+            });
+
+            return container.Locate<IMediator>();
+        }
+    }
+}
