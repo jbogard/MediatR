b2b0222231c3d09c48b2ace841cc02569c78fb4b
Jimmy Bogard <jimmy.bogard@gmail.com>
2022-01-06 07:34:45 -0600

    Creating a contracts package

---
+++ BuildContracts.ps1
@@ -0,0 +1,34 @@
+# Taken from psake https://github.com/psake/psake
+
+<#
+.SYNOPSIS
+  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+  to see if an error occcured. If an error is detected then an exception is thrown.
+  This function allows you to run command-line programs without having to
+  explicitly check the $lastexitcode variable.
+.EXAMPLE
+  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+function Exec
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+$artifacts = ".\artifacts"
+$contracts = ".\src\MediatR.Contracts\MediatR.Contracts.csproj"
+
+if(Test-Path $artifacts) { Remove-Item $artifacts -Force -Recurse }
+
+exec { & dotnet clean $contracts -c Release }
+
+exec { & dotnet build $contracts -c Release -p:ContinuousIntegrationBuild=true }
+
+exec { & dotnet pack $contracts -c Release -o $artifacts --no-build }
