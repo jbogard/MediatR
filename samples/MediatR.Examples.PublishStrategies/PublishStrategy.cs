d1d4b06da6f80038e8e6c2647eb6d4c0fca8ccb0
Luciano Delucchi <579642+lucianodelucchi@users.noreply.github.com>
2022-08-18 14:15:52 +0200

    chore: fix typos

--- samples/MediatR.Examples.PublishStrategies/PublishStrategy.cs
+++ samples/MediatR.Examples.PublishStrategies/PublishStrategy.cs
@@ -21,17 +21,17 @@ public enum PublishStrategy
     Async = 2,
 
     /// <summary>
-    /// Run each notification handler on it's own thread using Task.Run(). Returns immediately and does not wait for any handlers to finish. Note that you cannot capture any exceptions, even if you await the call to Publish.
+    /// Run each notification handler on its own thread using Task.Run(). Returns immediately and does not wait for any handlers to finish. Note that you cannot capture any exceptions, even if you await the call to Publish.
     /// </summary>
     ParallelNoWait = 3,
 
     /// <summary>
-    /// Run each notification handler on it's own thread using Task.Run(). Returns when all threads (handlers) are finished. In case of any exception(s), they are captured in an AggregateException by Task.WhenAll.
+    /// Run each notification handler on its own thread using Task.Run(). Returns when all threads (handlers) are finished. In case of any exception(s), they are captured in an AggregateException by Task.WhenAll.
     /// </summary>
     ParallelWhenAll = 4,
 
     /// <summary>
-    /// Run each notification handler on it's own thread using Task.Run(). Returns when any thread (handler) is finished. Note that you cannot capture any exceptions (See msdn documentation of Task.WhenAny)
+    /// Run each notification handler on its own thread using Task.Run(). Returns when any thread (handler) is finished. Note that you cannot capture any exceptions (See msdn documentation of Task.WhenAny)
     /// </summary>
     ParallelWhenAny = 5,
 }
