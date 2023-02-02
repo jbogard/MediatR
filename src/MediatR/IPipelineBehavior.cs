c0581f21eaa5379cdb7dfe788d8e1ae2d1d8220d
Darjan Bogdan <darjan@my-clay.com>
2023-02-02 14:42:08 +0100

    Change type constraint of pipeline behavior to allow easier chaining

--- src/MediatR/IPipelineBehavior.cs
+++ src/MediatR/IPipelineBehavior.cs
@@ -17,7 +17,7 @@ public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
 /// </summary>
 /// <typeparam name="TRequest">Request type</typeparam>
 /// <typeparam name="TResponse">Response type</typeparam>
-public interface IPipelineBehavior<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+public interface IPipelineBehavior<in TRequest, TResponse> where TRequest : IBaseRequest
 {
     /// <summary>
     /// Pipeline handler. Perform any additional behavior and await the <paramref name="next"/> delegate as necessary
