92791561e44aea4cc6c3d1d5c1e80d519d16e76c
pauldin91 <pauldin91@gmail.com>
2023-11-02 12:27:01 +0200

    Replace activator completely from methods CreateStream for object request and Send for object request

--- src/MediatR/Mediator.cs
+++ src/MediatR/Mediator.cs
@@ -101,9 +101,9 @@ public class Mediator : IMediator
                 var responseType = requestInterfaceType.GetGenericArguments()[0];
                 wrapperType = typeof(RequestHandlerWrapperImpl<,>).MakeGenericType(requestType, responseType);
             }
-
-            var wrapper = Activator.CreateInstance(wrapperType) ?? throw new InvalidOperationException($"Could not create wrapper for type {requestType}");
-            return (RequestHandlerBase) wrapper;
+            var ctor = wrapperType.GetConstructors().First();
+            var wrapper = Expression.Lambda<Func<RequestHandlerBase>>(Expression.New(ctor)).Compile() ?? throw new InvalidOperationException($"Could not create wrapper for type {requestType}");
+            return wrapper();
         });
 
         // call via dynamic dispatch to avoid calling through reflection for performance reasons
@@ -188,8 +188,9 @@ public class Mediator : IMediator
 
             var responseType = requestInterfaceType.GetGenericArguments()[0];
             var wrapperType = typeof(StreamRequestHandlerWrapperImpl<,>).MakeGenericType(requestType, responseType);
-            var wrapper = Activator.CreateInstance(wrapperType) ?? throw new InvalidOperationException($"Could not create wrapper for type {requestType}");
-            return (StreamRequestHandlerBase) wrapper;
+            var ctor = wrapperType.GetConstructors().First();
+            var wrapper = Expression.Lambda<Func<StreamRequestHandlerBase>>(Expression.New(ctor)).Compile() ?? throw new InvalidOperationException($"Could not create wrapper for type {requestType}");
+            return wrapper();
         });
 
         var items = handler.Handle(request, _serviceProvider, cancellationToken);
