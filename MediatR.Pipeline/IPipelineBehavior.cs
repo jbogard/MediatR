c40c2e650a68b8aff508b1483f0ae88a129c78c6
Mayron <mjr_4077@hotmail.co.uk>
2024-04-10 10:41:39 +0100

    Added 2 new abstraction projects

---
+++ MediatR.Pipeline/IPipelineBehavior.cs
@@ -0,0 +1,30 @@
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatR;
+
+/// <summary>
+/// Represents an async continuation for the next task to execute in the pipeline
+/// </summary>
+/// <typeparam name="TResponse">Response type</typeparam>
+/// <returns>Awaitable task returning a <typeparamref name="TResponse"/></returns>
+public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
+
+/// <summary>
+/// Pipeline behavior to surround the inner handler.
+/// Implementations add additional behavior and await the next delegate.
+/// </summary>
+/// <typeparam name="TRequest">Request type</typeparam>
+/// <typeparam name="TResponse">Response type</typeparam>
+public interface IPipelineBehavior<in TRequest, TResponse> where TRequest : notnull
+{
+    /// <summary>
+    /// Pipeline handler. Perform any additional behavior and await the <paramref name="next"/> delegate as necessary
+    /// </summary>
+    /// <param name="request">Incoming request</param>
+    /// <param name="next">Awaitable delegate for the next action in the pipeline. Eventually this delegate represents the handler.</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>Awaitable task returning the <typeparamref name="TResponse"/></returns>
+    Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken);
+}
