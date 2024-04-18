1552d92a35081119a5fae9454b74b56ffdf046f6
zach painter <zachpainter77@gmail.com>
2024-04-17 22:21:35 -0700

    remove Lamar Dependency

--- test/MediatR.Tests/SendTests.cs
+++ test/MediatR.Tests/SendTests.cs
@@ -5,7 +5,6 @@ namespace MediatR.Tests;
 using System;
 using System.Threading.Tasks;
 using Shouldly;
-using Lamar;
 using Xunit;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -109,21 +108,7 @@ public class SendTests
     [Fact]
     public async Task Should_resolve_main_handler()
     {
-        var container = new Container(cfg =>
-        {
-            cfg.Scan(scanner =>
-            {
-                scanner.AssemblyContainingType(typeof(PublishTests));
-                scanner.IncludeNamespaceContainingType<Ping>();
-                scanner.WithDefaultConventions();
-                scanner.AddAllTypesOf(typeof(IRequestHandler<,>));
-            });
-            cfg.For<IMediator>().Use<Mediator>();
-        });
-
-        var mediator = container.GetInstance<IMediator>();
-
-        var response = await mediator.Send(new Ping { Message = "Ping" });
+        var response = await _mediator.Send(new Ping { Message = "Ping" });
 
         response.Message.ShouldBe("Ping Pong");
     }
@@ -131,48 +116,16 @@ public class SendTests
     [Fact]
     public async Task Should_resolve_main_void_handler()
     {
-        var dependency = new Dependency();
+        await _mediator.Send(new VoidPing());
 
-        var container = new Container(cfg =>
-        {
-            cfg.Scan(scanner =>
-            {
-                scanner.AssemblyContainingType(typeof(PublishTests));
-                scanner.IncludeNamespaceContainingType<Ping>();
-                scanner.WithDefaultConventions();
-                scanner.AddAllTypesOf(typeof(IRequestHandler<>));
-                scanner.AddAllTypesOf(typeof(IRequestHandler<,>));
-            });
-            cfg.ForSingletonOf<Dependency>().Use(dependency);
-            cfg.For<IMediator>().Use<Mediator>();
-        });
-
-        var mediator = container.GetInstance<IMediator>();
-
-        await mediator.Send(new VoidPing());
-
-        dependency.Called.ShouldBeTrue();
+        _dependency.Called.ShouldBeTrue();
     }
 
     [Fact]
     public async Task Should_resolve_main_handler_via_dynamic_dispatch()
     {
-        var container = new Container(cfg =>
-        {
-            cfg.Scan(scanner =>
-            {
-                scanner.AssemblyContainingType(typeof(PublishTests));
-                scanner.IncludeNamespaceContainingType<Ping>();
-                scanner.WithDefaultConventions();
-                scanner.AddAllTypesOf(typeof(IRequestHandler<,>));
-            });
-            cfg.For<IMediator>().Use<Mediator>();
-        });
-
-        var mediator = container.GetInstance<IMediator>();
-
         object request = new Ping { Message = "Ping" };
-        var response = await mediator.Send(request);
+        var response = await _mediator.Send(request);
 
         var pong = response.ShouldBeOfType<Pong>();
         pong.Message.ShouldBe("Ping Pong");
@@ -181,50 +134,18 @@ public class SendTests
     [Fact]
     public async Task Should_resolve_main_void_handler_via_dynamic_dispatch()
     {
-        var dependency = new Dependency();
-
-        var container = new Container(cfg =>
-        {
-            cfg.Scan(scanner =>
-            {
-                scanner.AssemblyContainingType(typeof(PublishTests));
-                scanner.IncludeNamespaceContainingType<Ping>();
-                scanner.WithDefaultConventions();
-                scanner.AddAllTypesOf(typeof(IRequestHandler<>));
-                scanner.AddAllTypesOf(typeof(IRequestHandler<,>));
-            });
-            cfg.ForSingletonOf<Dependency>().Use(dependency);
-            cfg.For<IMediator>().Use<Mediator>();
-        });
-
-        var mediator = container.GetInstance<IMediator>();
-
         object request = new VoidPing();
-        var response = await mediator.Send(request);
+        var response = await _mediator.Send(request);
 
         response.ShouldBeOfType<Unit>();
 
-        dependency.Called.ShouldBeTrue();
+        _dependency.Called.ShouldBeTrue();
     }
 
     [Fact]
     public async Task Should_resolve_main_handler_by_specific_interface()
     {
-        var container = new Container(cfg =>
-        {
-            cfg.Scan(scanner =>
-            {
-                scanner.AssemblyContainingType(typeof(PublishTests));
-                scanner.IncludeNamespaceContainingType<Ping>();
-                scanner.WithDefaultConventions();
-                scanner.AddAllTypesOf(typeof(IRequestHandler<,>));
-            });
-            cfg.For<ISender>().Use<Mediator>();
-        });
-
-        var mediator = container.GetInstance<ISender>();
-
-        var response = await mediator.Send(new Ping { Message = "Ping" });
+        var response = await _mediator.Send(new Ping { Message = "Ping" });
 
         response.Message.ShouldBe("Ping Pong");
     }
@@ -232,44 +153,18 @@ public class SendTests
     [Fact]
     public async Task Should_resolve_main_handler_by_given_interface()
     {
-        var dependency = new Dependency();
-        var container = new Container(cfg =>
-        {
-            cfg.Scan(scanner =>
-            {
-                scanner.AssemblyContainingType(typeof(PublishTests));
-                scanner.IncludeNamespaceContainingType<VoidPing>();
-                scanner.WithDefaultConventions();
-                scanner.AddAllTypesOf(typeof(IRequestHandler<>));
-            });
-            cfg.ForSingletonOf<Dependency>().Use(dependency);
-            cfg.For<ISender>().Use<Mediator>();
-        });
-
-        var mediator = container.GetInstance<ISender>();
-
         // wrap requests in an array, so this test won't break on a 'replace with var' refactoring
         var requests = new IRequest[] { new VoidPing() };
-        await mediator.Send(requests[0]);
+        await _mediator.Send(requests[0]);
 
-        dependency.Called.ShouldBeTrue();
+        _dependency.Called.ShouldBeTrue();
     }
 
     [Fact]
-    public async Task Should_raise_execption_on_null_request()
-    {
-        var container = new Container(cfg =>
-        {
-            cfg.For<ISender>().Use<Mediator>();
-        });
-
-        var mediator = container.GetInstance<ISender>();
-
-        await Should.ThrowAsync<ArgumentNullException>(async () => await mediator.Send(default!));
-    }
+    public Task Should_raise_execption_on_null_request() => Should.ThrowAsync<ArgumentNullException>(async () => await _mediator.Send(default!));
 
     [Fact]
-    public async Task Should_resolve_generic_handler_by_given_interface()
+    public async Task Should_resolve_generic_handler()
     {
         var request = new GenericPing<Pong> { Pong = new Pong { Message = "Ping" } };
         var result = await _mediator.Send(request);
@@ -281,7 +176,7 @@ public class SendTests
     }
 
     [Fact]
-    public async Task Should_resolve_generic_void_handler_by_given_interface()
+    public async Task Should_resolve_generic_void_handler()
     {
         var request = new VoidGenericPing<Pong>();
         await _mediator.Send(request);
