5386e3c96d29343dbe9765f122e47c3d8f50af1a
Jithin Johnson <jithu7432@gmail.com>
2024-09-22 22:23:20 +0530

    Added tests for DryIocDependencyInjection

---
+++ test/MediatR.DependencyInjectionTests/DryIocDependencyInjectionTests.cs
@@ -0,0 +1,9 @@
+﻿using MediatR.DependencyInjectionTests.Abstractions;
+using MediatR.DependencyInjectionTests.Providers;
+
+namespace MediatR.DependencyInjectionTests;
+
+public class DryIocDependencyInjectionTests : BaseAssemblyResolutionTests
+{
+    public DryIocDependencyInjectionTests() : base(new DryIocServiceProviderFixture()) { }
+}
