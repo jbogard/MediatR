369442aebe0a783f6ea28753d5660ccd39a52757
Jithin Johnson <jithu7432@gmail.com>
2024-09-22 22:23:20 +0530

    Added tests for DryIocDependencyInjection

---
+++ test/MediatR.DependencyInjectionTests/Providers/DryIocServiceProviderFixture.cs
@@ -0,0 +1,19 @@
+﻿using DryIoc;
+using DryIoc.Microsoft.DependencyInjection;
+using MediatR.DependencyInjectionTests.Abstractions;
+
+namespace MediatR.DependencyInjectionTests.Providers;
+
+public class DryIocServiceProviderFixture : BaseServiceProviderFixture
+{
+    public override IServiceProvider Provider
+    {
+        get
+        {
+            var container = new Container();
+            container.RegisterMany(new[] { typeof(IMediator).GetAssembly(), typeof(Pong).GetAssembly() }, Registrator.Interfaces);
+            container.Register<IMediator, Mediator>(made: Made.Of(() => new Mediator(Arg.Of<IServiceProvider>())));
+            return container.BuildServiceProvider();
+        }
+    }
+}
